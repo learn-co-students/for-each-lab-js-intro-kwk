fcd378946f66ccd6bac8e1d114ba2ded801e59e1
Divya Lala <divya.h.lala@gmail.com>
2017-04-01 16:55:44 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function iterativeLog(array){
+  array.forEach(console.log(`${i}: ${array[i]}`);)
+}
+
+function iterate(callback){
+  var array = ['a', 'b', 'c'];
+  array.forEach(callback)
+}
+
+function doToArray(array, callback){
+  array.forEach(callback)
+}
